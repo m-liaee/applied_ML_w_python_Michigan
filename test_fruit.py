d6b42c48cd88e1ca91f17328c3b34c14abdcfba8
Mehraneh <m.liaee2050@gmail.com>
2019-08-08 20:06:09 -0400

    split data to train and test

--- test_fruit.py
+++ test_fruit.py
@@ -7,3 +7,10 @@ from sklearn.model_selection import train_test_split
 fruits = pd.read_table('fruit_data_with_colors.txt')
 
 fruits.head()
+
+fruits.shape 
+
+X = fruits[['mass', 'width', 'height']]
+Y = fruits['fruit_label']
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, random_state = 0)
+
